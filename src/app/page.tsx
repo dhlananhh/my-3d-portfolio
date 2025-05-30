d5f29ec2dfbfb58860df3766313073e698da935a
Lan Anh <dhlananh2309@gmail.com>
2025-05-30 10:25:00 +0700

    feat: add scroll progress component to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,12 @@ import SkillsSection from "@/components/sections/SkillsSection";
 import TestimonialsSection from "@/components/sections/TestimonialsSection";
 import ResumeSection from "@/components/sections/ResumeSection";
 import ContactSection from "@/components/sections/ContactSection";
+import { ScrollProgress } from "@/components/ScrollProgress";
 
 export default function HomePage() {
   return (
     <>
+      <ScrollProgress />
       <HeroSection />
       <AboutMeSection />
       <ProjectsSection />
