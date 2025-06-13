67fc88bfdaf0bff141745af253d16c04740867ba
Lan Anh <dhlananh2309@gmail.com>
2025-06-13 13:18:31 +0700

    feat: import and use Images object in `data.ts`

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,9 +1,11 @@
+import { Images } from "@/lib/images";
+
 export interface Project {
   id: string;
   title: string;
   description: string;
   shortDescription: string;
-  imageUrl: string;
+  imageUrl: string | Object;
   videoUrl?: string;
   githubUrl: string;
   tools: string[];
@@ -30,7 +32,7 @@ export const projectsData: Project[] = [
     slug: "ecommerce-website",
     shortDescription: "Online ecommerce website, with chatbot integration.",
     description: "This project serves as one of my personal projects.",
-    imageUrl: "/images/ecommerce-project.png",
+    imageUrl: Images.EcommerceProject,
     githubUrl: "https://github.com/dhlananhh/quickcart/",
     tools: [ "Next.js", "TailwindCSS", "Clerk", "MongoDB" ],
     category: "Projects",
@@ -44,7 +46,7 @@ export const projectsData: Project[] = [
     slug: "online-bookstore-project",
     shortDescription: "Online bookstore system, with chatbot integration.",
     description: "This project serves as a final report exam on software architecture. I am a member of this project, as a frontend coder, building and integrating chatbot into the bookstore system.",
-    imageUrl: "/images/bookstore-project.png",
+    imageUrl: Images.BookstoreProject,
     githubUrl: "https://github.com/minhlq2003/bookstore-frontend-nextjs",
     tools: [ "Next.js", "TailwindCSS", "TypeScript" ],
     category: "Projects",
@@ -58,7 +60,7 @@ export const projectsData: Project[] = [
     slug: "online-bookstore-project",
     shortDescription: "Online bookstore system, with chatbot integration.",
     description: "This project serves as a final report exam on software architecture. I am a member of this project, as a frontend coder, building and integrating chatbot into the bookstore system.",
-    imageUrl: "/images/bookstore-project.png",
+    imageUrl: Images.BookstoreProject,
     githubUrl: "https://github.com/HieuTrungMc/bookstore-backend-nodejs",
     tools: [ "Node.js", "Express.js", "Prisma", "MariaDB" ],
     category: "Projects",
@@ -67,25 +69,11 @@ export const projectsData: Project[] = [
   },
   {
     id: "4",
-    title: "DeepSeek Clone Full-stack Website Project",
-    slug: "deepseek-clone",
-    shortDescription: "DeepSeek clone website",
-    description: "This project serves as one of my personal projects.",
-    imageUrl: "/images/deepseek-clone-project.png",
-    githubUrl: "https://github.com/dhlananhh/deepseek-clone",
-    tools: [ "Next.js", "TailwindCSS", "TypeScript" ],
-    category: "Projects",
-    projectStartDate: new Date("2025-01-11"),
-    projectStatus: "In Progress",
-    liveDemoUrl: "https://deepseek-clone-gold.vercel.app/"
-  },
-  {
-    id: "5",
     title: "Portfolio Website Project",
     slug: "portfolio-website-project",
     shortDescription: "My Portfolio Website",
     description: "This project serves as one of my personal projects.",
-    imageUrl: "/images/my-portfolio-website-project.png",
+    imageUrl: Images.MyPortfolioWebsiteProject,
     githubUrl: "https://github.com/dhlananhh/my-3d-portfolio",
     tools: [ "Next.js", "TailwindCSS", "TypeScript", "Radix UI", "Shadcn UI" ],
     category: "Projects",
@@ -94,12 +82,12 @@ export const projectsData: Project[] = [
     liveDemoUrl: "https://my-3d-portfolio-wine.vercel.app/"
   },
   {
-    id: "6",
+    id: "5",
     title: "RAG System with Gemini and MongoDB Atlas",
     slug: "rag-system-with-gemini-and-mongodb-atlas",
     shortDescription: "RAG (Retrieval-Augmented Generation) system using Google's Gemini for embeddings and text generation, with MongoDB Atlas serving as the vector database for efficient information storage and retrieval.",
     description: "This project serves as one of my personal projects.",
-    imageUrl: "/images/my-portfolio-website-project.png",
+    imageUrl: Images.RAG_System,
     githubUrl: "https://github.com/dhlananhh/Build_RAG_System_with_Gemini_and_MongoDB",
     tools: [ "Python", "google-generativeai", "mongodb" ],
     category: "Projects",
@@ -107,18 +95,32 @@ export const projectsData: Project[] = [
     projectStatus: "Completed",
   },
   {
-    id: "7",
+    id: "6",
     title: "RAG System with Gemini and ChromaDB",
     slug: "rag-system-with-gemini-and-mongodb-atlas",
     shortDescription: "RAG (Retrieval-Augmented Generation) system using Google's Gemini Flash model and the ChromaDB vector database to answer questions based on the content of a PDF document.",
     description: "This project serves as one of my personal projects.",
-    imageUrl: "/images/my-portfolio-website-project.png",
+    imageUrl: Images.RAG_System,
     githubUrl: "https://github.com/dhlananhh/Build_RAG_System_with_Gemini_and_ChromaDB",
     tools: [ "Python", "google-generativeai", "chromadb" ],
     category: "Projects",
     projectStartDate: new Date("2025-01-11"),
     projectStatus: "Completed",
   },
+  // {
+  //   id: "4",
+  //   title: "DeepSeek Clone Full-stack Website Project",
+  //   slug: "deepseek-clone",
+  //   shortDescription: "DeepSeek clone website",
+  //   description: "This project serves as one of my personal projects.",
+  //   imageUrl: "/images/deepseek-clone-project.png",
+  //   githubUrl: "https://github.com/dhlananhh/deepseek-clone",
+  //   tools: [ "Next.js", "TailwindCSS", "TypeScript" ],
+  //   category: "Projects",
+  //   projectStartDate: new Date("2025-01-11"),
+  //   projectStatus: "In Progress",
+  //   liveDemoUrl: "https://deepseek-clone-gold.vercel.app/"
+  // },
 ];
 
 export const testimonialsData: Testimonial[] = [
